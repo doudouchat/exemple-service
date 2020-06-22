75ba64fea3849be81e4045074cb5a8a264c4da30
doudouchat <baraultb@hotmail.com>
2020-06-22 23:03:45 +0200

    remove code.revelc.net.zookeeper-maven-plugin

--- exemple-service-api/src/main/java/com/exemple/service/api/core/ApiServerApplication.java
+++ exemple-service-api/src/main/java/com/exemple/service/api/core/ApiServerApplication.java
@@ -1,13 +1,17 @@
 package com.exemple.service.api.core;
 
+import org.apache.curator.test.TestingServer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -33,6 +37,24 @@ public class ApiServerApplication extends SpringBootServletInitializer {
     @Import(EventConfiguration.class)
     @ConditionalOnProperty(value = { "kafka.bootstrap-servers", "topic" }, prefix = "event")
     public static class EventApiConfiguration {
+
+    }
+
+    @Configuration
+    @ConditionalOnClass(TestingServer.class)
+    public static class ZookeeperConfiguration {
+
+        private final int port;
+
+        public ZookeeperConfiguration(@Value("${api.embedded.zookeeper.port:-1}") int port) {
+            this.port = port;
+        }
+
+        @Bean(initMethod = "start", destroyMethod = "stop")
+        public TestingServer embeddedZookeeper() throws Exception {
+
+            return new TestingServer(port, false);
+        }
 
     }
 
