40315f54f13d559012f779a93d3a26d6e208c782
doudouchat <baraultb@hotmail.com>
2020-06-26 19:27:02 +0100

    add event configuration

--- exemple-service-api/src/main/java/com/exemple/service/api/core/ApiServerApplication.java
+++ exemple-service-api/src/main/java/com/exemple/service/api/core/ApiServerApplication.java
@@ -3,14 +3,17 @@ package com.exemple.service.api.core;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import com.exemple.service.application.core.ApplicationConfiguration;
 import com.exemple.service.customer.core.CustomerConfiguration;
+import com.exemple.service.event.core.EventConfiguration;
 import com.exemple.service.resource.core.ResourceConfiguration;
 import com.exemple.service.schema.core.SchemaConfiguration;
 import com.exemple.service.store.core.StoreConfiguration;
@@ -26,4 +29,10 @@ public class ApiServerApplication extends SpringBootServletInitializer {
         return application.sources(ApiServerApplication.class);
     }
 
+    @Configuration
+    @Import(EventConfiguration.class)
+    @ConditionalOnProperty(value = { "kafka.bootstrap-servers", "topic" }, prefix = "event")
+    public static class EventApiConfiguration {
+
+    }
 }
