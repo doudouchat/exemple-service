3f8532f18e8bb5b48c37a823e87fb0c60476581a
doudouchat <baraultb@hotmail.com>
2020-08-27 23:18:55 +0200

    external cassandra configuration

--- exemple-service-resource/src/main/java/com/exemple/service/resource/core/cassandra/ResourceCassandraConfiguration.java
+++ exemple-service-resource/src/main/java/com/exemple/service/resource/core/cassandra/ResourceCassandraConfiguration.java
@@ -1,16 +1,17 @@
 package com.exemple.service.resource.core.cassandra;
 
-import java.net.InetSocketAddress;
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
 
 import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
 import com.datastax.oss.driver.api.core.type.codec.ExtraTypeCodecs;
 import com.datastax.oss.driver.api.core.type.codec.registry.MutableCodecRegistry;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -18,26 +19,19 @@ import com.fasterxml.jackson.databind.JsonNode;
 @Configuration
 public class ResourceCassandraConfiguration {
 
-    private final String[] addresses;
+    private final File cassandraResource;
 
-    private final int port;
-
-    private final String localDataCenter;
-
-    public ResourceCassandraConfiguration(@Value("${resource.cassandra.addresses}") String[] addresses, @Value("${resource.cassandra.port}") int port,
-            @Value("${resource.cassandra.local_data_center}") String localDataCenter) {
-
-        this.addresses = addresses.clone();
-        this.port = port;
-        this.localDataCenter = localDataCenter;
+    public ResourceCassandraConfiguration(@Value("${resource.cassandra.resource_configuration}") String cassandraResource)
+            throws FileNotFoundException {
+        this.cassandraResource = ResourceUtils.getFile(cassandraResource);
     }
 
     @Bean
     public CqlSession session() {
 
-        return CqlSession.builder().withLocalDatacenter(localDataCenter)
-                .addContactPoints(Arrays.stream(addresses).map((String address) -> new InetSocketAddress(address, port)).collect(Collectors.toList()))
-                .build();
+        DriverConfigLoader loader = DriverConfigLoader.fromFile(cassandraResource);
+
+        return CqlSession.builder().withConfigLoader(loader).build();
     }
 
     @PostConstruct
