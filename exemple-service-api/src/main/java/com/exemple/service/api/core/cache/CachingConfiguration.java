e1476cb1d47de95227f714c65d766455731aa7a2
doudouchat <38097871+doudouchat@users.noreply.github.com>
2021-01-06 14:21:21 +0100

    custom cache configuration

---
+++ exemple-service-api/src/main/java/com/exemple/service/api/core/cache/CachingConfiguration.java
@@ -0,0 +1,30 @@
+package com.exemple.service.api.core.cache;
+
+import java.lang.reflect.Method;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.interceptor.SimpleKeyGenerator;
+import org.springframework.context.annotation.Configuration;
+
+import com.exemple.service.resource.core.ResourceExecutionContext;
+
+@Configuration
+public class CachingConfiguration extends CachingConfigurerSupport {
+
+    @Override
+    public KeyGenerator keyGenerator() {
+        return new CustomKeyGenerator();
+    }
+
+    private static class CustomKeyGenerator implements KeyGenerator {
+
+        @Override
+        public Object generate(Object o, Method method, Object... params) {
+
+            return SimpleKeyGenerator.generateKey(ArrayUtils.addFirst(params, ResourceExecutionContext.get().keyspace()));
+        }
+
+    }
+}
