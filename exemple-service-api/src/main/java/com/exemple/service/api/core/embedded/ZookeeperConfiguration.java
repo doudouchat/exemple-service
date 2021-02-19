7da60a8031629ba6b33e09804a8ef9aebcb03988
doudouchat <baraultb@hotmail.com>
2021-02-19 09:04:55 +0000

    api embedded module

--- exemple-service-api/src/main/java/com/exemple/service/api/core/embedded/ZookeeperConfiguration.java
+++ exemple-service-api/src/main/java/com/exemple/service/api/core/embedded/ZookeeperConfiguration.java
@@ -1,6 +1,8 @@
 package com.exemple.service.api.core.embedded;
 
 import org.apache.curator.test.TestingServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
@@ -10,14 +12,18 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnClass(TestingServer.class)
 public class ZookeeperConfiguration {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ZookeeperConfiguration.class);
+
     private final int port;
 
-    public ZookeeperConfiguration(@Value("${api.embedded.zookeeper.port:-1}") int port) {
+    public ZookeeperConfiguration(@Value("${zookeeper.embedded.port:-1}") int port) {
         this.port = port;
     }
 
     @Bean(initMethod = "start", destroyMethod = "stop")
     public TestingServer embeddedZookeeper() throws Exception {
+
+        LOG.info("STARTING EMBEDDED ZOOKEEPER");
 
         return new TestingServer(port, false);
     }
