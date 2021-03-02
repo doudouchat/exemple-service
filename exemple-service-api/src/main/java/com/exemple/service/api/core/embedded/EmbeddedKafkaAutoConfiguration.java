90e42adad591c4641e0acadf5298ed571ebdef09
doudouchat <baraultb@hotmail.com>
2021-03-02 12:01:37 +0000

    embedded kafka auto configuration

--- exemple-service-api/src/main/java/com/exemple/service/api/core/embedded/EmbeddedKafkaAutoConfiguration.java
+++ exemple-service-api/src/main/java/com/exemple/service/api/core/embedded/EmbeddedKafkaAutoConfiguration.java
@@ -2,11 +2,10 @@ package com.exemple.service.api.core.embedded;
 
 import java.util.UUID;
 
-import javax.annotation.PostConstruct;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,21 +15,23 @@ import kafka.server.KafkaConfig;
 
 @Configuration
 @ConditionalOnProperty(value = { "port", "dir", "defaultTopic" }, prefix = "kafka.embedded")
-public class KafkaConfiguration {
+@ConditionalOnClass(EmbeddedKafkaBroker.class)
+public class EmbeddedKafkaAutoConfiguration {
 
-    private static final Logger LOG = LoggerFactory.getLogger(KafkaConfiguration.class);
+    private static final Logger LOG = LoggerFactory.getLogger(EmbeddedKafkaAutoConfiguration.class);
 
     private final int kafkaPort;
 
     private final String logDir;
 
-    private final String defaultTopic;
+    private final String[] defaultTopic;
 
-    public KafkaConfiguration(@Value("${kafka.embedded.port}") int kafkaPort, @Value("${kafka.embedded.dir}") String logDir,
-            @Value("${kafka.embedded.defaultTopic}") String defaultTopic) {
+    public EmbeddedKafkaAutoConfiguration(@Value("${kafka.embedded.port:0}") int kafkaPort,
+            @Value("${kafka.embedded.dir:#{systemProperties['java.io.tmpdir']}}") String logDir,
+            @Value("${kafka.embedded.defaultTopic:...}") String[] defaultTopic) {
         this.kafkaPort = kafkaPort;
         this.logDir = logDir;
-        this.defaultTopic = defaultTopic;
+        this.defaultTopic = defaultTopic.clone();
 
     }
 
@@ -41,15 +42,9 @@ public class KafkaConfiguration {
                 logDir + "/" + UUID.randomUUID());
         embeddedKafka.kafkaPorts(kafkaPort);
 
-        return embeddedKafka;
-    }
-
-    @PostConstruct
-    public void startKafka() {
-
         LOG.info("STARTING EMBEDDED KAFKA");
 
-        embeddedKafka().afterPropertiesSet();
+        return embeddedKafka;
     }
 
 }
