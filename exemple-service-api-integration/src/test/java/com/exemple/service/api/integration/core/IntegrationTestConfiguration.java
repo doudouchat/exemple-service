146800e8db52498c1d5e1fbcdcbca01b0a914fe2
doudouchat <38097871+doudouchat@users.noreply.github.com>
2021-05-04 10:52:11 +0200

    docker compose no secure (#70)

--- exemple-service-api-integration/src/test/java/com/exemple/service/api/integration/core/IntegrationTestConfiguration.java
+++ exemple-service-api-integration/src/test/java/com/exemple/service/api/integration/core/IntegrationTestConfiguration.java
@@ -3,6 +3,7 @@ package com.exemple.service.api.integration.core;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -35,10 +36,10 @@ public class IntegrationTestConfiguration {
     }
 
     @Bean
-    public HazelcastInstance hazelcastInstance() throws UnknownHostException {
+    public HazelcastInstance hazelcastInstance(@Value("${hazelcast.port:5701}") int port) throws UnknownHostException {
 
         ClientConfig config = new ClientConfig();
-        config.getNetworkConfig().addAddress(InetAddress.getLocalHost().getHostAddress() + ":5701");
+        config.getNetworkConfig().addAddress(InetAddress.getLocalHost().getHostAddress() + ":" + port);
 
         return HazelcastClient.newHazelcastClient(config);
     }
