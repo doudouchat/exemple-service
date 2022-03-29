622d74e2966ab08c69c7f236aa5882965d3d5d16
doudouchat <baraultb@hotmail.com>
2022-03-29 13:32:54 +0100

    lombok in ValidationExceptionModel

--- exemple-service-schema/src/main/java/com/exemple/service/schema/common/exception/ValidationExceptionModel.java
+++ exemple-service-schema/src/main/java/com/exemple/service/schema/common/exception/ValidationExceptionModel.java
@@ -1,17 +1,17 @@
 package com.exemple.service.schema.common.exception;
 
 import java.io.Serializable;
-import java.util.Objects;
-
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.springframework.util.Assert;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.core.JsonPointer;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
+@EqualsAndHashCode
 public class ValidationExceptionModel implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -19,6 +19,7 @@ public class ValidationExceptionModel implements Serializable {
     private final String path;
 
     @JsonIgnore
+    @EqualsAndHashCode.Exclude
     private final JsonPointer pointer;
 
     private final String code;
@@ -30,23 +31,6 @@ public class ValidationExceptionModel implements Serializable {
         this.path = this.pointer.toString();
         this.code = code;
         this.message = message;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(code, message, path);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        Assert.notNull(obj, "obj is required");
-        ValidationExceptionModel other = (ValidationExceptionModel) obj;
-        return new EqualsBuilder().append(code, other.code).append(message, other.message).append(path, other.path).isEquals();
-    }
-
-    @Override
-    public String toString() {
-        return String.format("ValidationExceptionModel [path=%s, pointer=%s, code=%s, message=%s]", path, pointer, code, message);
     }
 
 }
