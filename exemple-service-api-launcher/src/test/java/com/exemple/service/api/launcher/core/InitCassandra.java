1c0ab8b2771c51fccfc8e0f0510614013db29c53
doudouchat <baraultb@hotmail.com>
2022-08-04 23:02:22 +0200

    move embedded cassandra to container

--- exemple-service-api-launcher/src/test/java/com/exemple/service/api/launcher/core/InitCassandra.java
+++ exemple-service-api-launcher/src/test/java/com/exemple/service/api/launcher/core/InitCassandra.java
@@ -1,30 +1,29 @@
 package com.exemple.service.api.launcher.core;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import com.datastax.oss.driver.api.core.CqlSession;
-import com.github.nosan.embedded.cassandra.commons.FileSystemResource;
-import com.github.nosan.embedded.cassandra.commons.Resource;
-import com.github.nosan.embedded.cassandra.cql.CqlScript;
-import com.github.nosan.embedded.cassandra.cql.ResourceCqlScript;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.SneakyThrows;
 
 @Component
-@Slf4j
 public class InitCassandra {
 
     private final Resource[] scripts;
 
     private final CqlSession session;
 
-    public InitCassandra(CqlSession session, @Value("${cassandra.embedded.scripts:}") String... scripts) {
+    public InitCassandra(CqlSession session, @Value("${cassandra.scripts:}") String... scripts) {
         this.session = session;
         this.scripts = Arrays.stream(scripts).map(File::new).map(FileSystemResource::new).toArray(Resource[]::new);
 
@@ -33,10 +32,13 @@ public class InitCassandra {
     @PostConstruct
     public void initKeyspace() {
 
-        LOG.info("INIT EMBEDDED CASSANDRA");
+        Arrays.stream(scripts).flatMap((Resource script) -> Arrays.stream(splitScript(script))).forEach(session::execute);
 
-        Arrays.stream(scripts).map(ResourceCqlScript::new).forEach((CqlScript script) -> script.forEachStatement(session::execute));
+    }
 
+    @SneakyThrows
+    private static String[] splitScript(Resource script) {
+        return FileUtils.readFileToString(script.getFile(), StandardCharsets.UTF_8).trim().split(";");
     }
 
 }
