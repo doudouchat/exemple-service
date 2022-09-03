f1e74c1be58385abd4d74257dc6482eed62c724e
doudouchat <baraultb@hotmail.com>
2022-09-03 09:07:42 +0100

    remove property path in ValidationExceptionCause

--- exemple-service-schema/src/main/java/com/exemple/service/schema/common/exception/ValidationExceptionCause.java
+++ exemple-service-schema/src/main/java/com/exemple/service/schema/common/exception/ValidationExceptionCause.java
@@ -6,31 +6,27 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @ToString
 @EqualsAndHashCode
+@RequiredArgsConstructor
 public class ValidationExceptionCause {
 
-    private final String path;
-
     @JsonIgnore
-    @EqualsAndHashCode.Exclude
     private final JsonPointer pointer;
 
     private final String code;
 
+    @EqualsAndHashCode.Exclude
     private final String message;
 
     private final JsonNode value;
 
-    public ValidationExceptionCause(JsonPointer pointer, String code, String message, JsonNode value) {
-        this.pointer = pointer;
-        this.path = this.pointer.toString();
-        this.code = code;
-        this.message = message;
-        this.value = value;
+    public String getPath() {
+        return this.pointer.toString();
     }
 
 }
