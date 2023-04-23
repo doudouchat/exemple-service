f2e6d5769bfece827fb2e3fd3615a46b0981e796
doudouchat <baraultb@hotmail.com>
2023-04-23 12:51:43 +0100

    fix sonar code smell

--- exemple-service-resource/src/main/java/com/exemple/service/resource/common/util/JsonPatchUtils.java
+++ exemple-service-resource/src/main/java/com/exemple/service/resource/common/util/JsonPatchUtils.java
@@ -43,7 +43,7 @@ public final class JsonPatchUtils {
 
     }
 
-    public static boolean isRemoveOperation(JsonNode patch) {
+    public static <T extends JsonNode> boolean isRemoveOperation(T patch) {
 
         return "remove".equals(patch.get(OP).textValue());
     }
