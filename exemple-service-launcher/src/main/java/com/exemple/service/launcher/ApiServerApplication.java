4e6c7f373bca058d2fbaee56dd176dc28dc5d431
doudouchat <baraultb@hotmail.com>
2025-07-29 20:52:03 +0100

    upgrade spring boot 4.0.0

--- exemple-service-launcher/src/main/java/com/exemple/service/launcher/ApiServerApplication.java
+++ exemple-service-launcher/src/main/java/com/exemple/service/launcher/ApiServerApplication.java
@@ -1,10 +1,6 @@
 package com.exemple.service.launcher;
 
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
@@ -17,8 +13,7 @@ import com.exemple.service.resource.core.ResourceConfiguration;
 import com.exemple.service.schema.core.SchemaConfiguration;
 import com.exemple.service.store.core.StoreConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class,
-        CassandraAutoConfiguration.class })
+@SpringBootApplication
 @EnableCaching
 @Import({ SchemaConfiguration.class, ApplicationConfiguration.class, StoreConfiguration.class, ResourceConfiguration.class, ApiConfiguration.class })
 @PropertySource("classpath:default.properties")
